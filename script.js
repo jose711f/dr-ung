5af20903326cedf5d97626b45343114de184dc5d
anne3m40 <anne3m40@stud.kea.dk>
2020-12-03 18:51:10 +0100

    nav og footer hentes ind med js

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-window.addEventListener("load", klikKlar);
+document.addEventListener("DOMContentLoaded", start);
 
+function start() {
+
+    hentNav();
+    hentFooter();
+
+    klikKlar();
+
+}
+
+async function hentNav() {
+
+    //henter nav.html
+    const responseNav = await fetch("inc/nav.html");
+
+    //fortæller at indholdet i nav skal være text
+    const inclusionNav = await responseNav.text();
+
+    //indsætter nav.html ind i <nav></nav> på alle sider.
+    document.querySelector("nav").innerHTML = inclusionNav;
+
+}
 
 function klikKlar() {
     document.querySelector("#knap").addEventListener("click", visResultat);
@@ -8,5 +29,18 @@ function klikKlar() {
 function visResultat() {
     document.querySelector("#quiz-resultat").classList.remove("d-none");
     document.querySelector("#quiz-indhold").classList.add("d-none");
+
+}
+
+async function hentFooter() {
+
+    //henter footer.html
+    const responseFooter = await fetch("inc/footer.html");
+
+    //fortæller at indholdet i footer skal være text
+    const inclusionFooter = await responseFooter.text();
+
+    //indsætter footer.html ind i <footer></footer> på alle sider.
+    document.querySelector("footer").innerHTML = inclusionFooter;
 
 }
