c5cd6c308ffc81cfa82cd5ac0b6c733f14a1e078
anne3m40 <anne3m40@stud.kea.dk>
2020-12-06 10:56:51 +0100

    nav markør klar - tror jeg

--- script.js
+++ script.js
@@ -21,45 +21,63 @@ async function hentNav() {
     //indsætter nav.html ind i <nav></nav> på alle sider.
     document.querySelector("nav").innerHTML = inclusionNav;
 
-    klikNav();
-}
+    // klikNav();
 
-function klikNav() {
-    console.log("hej");
-
-    // document.querySelector(".nav-link").addEventListener("click", markerNav);
-
-    // sæt en eventlistner på alle
-    document.querySelectorAll(".nav-item").forEach((link) => {
-        //Send "e" med til din funktion (elementet du har klikket på"
-        link.addEventListener("click", function (e) {
-            markerNav(e);
-            console.log("send e med");
-        });
-    });
-
+    //markere i nav hvilken hviklen siden man er på
+    markerNav();
 }
 
 function markerNav() {
-    console.log("hej2");
-    // document.querySelector(".nav-link").classList.remove("nav-aktiv");
-    //  document.querySelector(this).classList.add("nav-aktiv");
 
-    //Fjern klassen på alle
-    document.querySelectorAll("li").forEach((link) => {
-        link.classList.remove("nav-aktiv");
-    });
+    //vi laver et array over alle sidernes ID som sidder på main:
+    let navs = ["forside", "serier", "musik", "videoer", "nyheder"];
 
-    //Sæt klassen på den du har klikket på
-    e.target.classList.add("nav-aktiv");
+
+    navs.forEach(nav => {
+
+        //variabel som henter IDer (da vi ændrede navnet på main ID'ere, undervejs, er der tilføjet "main-" det kunne man også gøre i arrayet. Men her kan man nøjes med at gøre det et sted. Er også smart il senere, da vi har brugt arrayet til også at finde den rigtige nav.)
+        let mainElement = document.getElementById("main-" + nav);
+
+        // If sætning. Hvis siden har et main ID'et fra arayet, finder den elementet, som har et ID der hedder nav- + array-element, som sidder i nav'en på nav-link, og adder derefter classen nav-aktiv, som markere sidewn vi er på.
+        if (mainElement != null) {
+            document.getElementById("nav-" + nav).classList.add("nav-aktiv");
+        }
+    })
+
 
 }
 
-function visResultat() {
-    document.querySelector("#quiz-resultat").classList.remove("d-none");
-    document.querySelector("#quiz-indhold").classList.add("d-none");
+//function klikNav() {
+console.log("hej");
 
-}
+// document.querySelector(".nav-link").addEventListener("click", markerNav);
+
+// sæt en eventlistner på alle
+// document.querySelectorAll(".nav-item").forEach((link) => {
+//Send "e" med til din funktion (elementet du har klikket på"
+//   link.addEventListener("click", function (e) {
+//     markerNav(e);
+//      console.log("send e med");
+// });
+//  });
+
+//}
+
+//function markerNav() {
+console.log("hej2");
+// document.querySelector(".nav-link").classList.remove("nav-aktiv");
+//  document.querySelector(this).classList.add("nav-aktiv");
+
+//Fjern klassen på alle
+//  document.querySelectorAll("li").forEach((link) => {
+//      link.classList.remove("nav-aktiv");
+//   });
+
+//Sæt klassen på den du har klikket på
+//   e.target.classList.add("nav-aktiv");
+
+//}
+
 
 //Kode til karusellen med 1 stor og halve er hentet fra https://stackoverflow.com/questions/20346847/bootstrap-carousel-showing-next-and-previous-image //
 function storKarusel() {
@@ -78,8 +96,15 @@ function storKarusel() {
     });
 }
 
+//til quiz knappen - viser resultater
 function klikKlar() {
     document.querySelector("#knap").addEventListener("click", visResultat);
+}
+
+function visResultat() {
+    document.querySelector("#quiz-resultat").classList.remove("d-none");
+    document.querySelector("#quiz-indhold").classList.add("d-none");
+
 }
 
 async function hentFooter() {
